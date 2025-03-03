32b7b68837f31182059de9835860eb333d821710
Vojta Vorel <vorelvo2@uhk.cz>
2025-03-03 12:00:52 +0100

    Parsování (testy prošly)

--- src/main/java/pro1/Fraction.java
+++ src/main/java/pro1/Fraction.java
@@ -1,5 +1,7 @@
 package pro1;
 
+import java.util.regex.Pattern;
+
 public class Fraction
 {
     long n; // čitatel
@@ -8,12 +10,38 @@ public class Fraction
     public Fraction(long n, long d) {
         long gcd = Utils.gcd(n,d);
         this.n = n / gcd;
-        this. d = d / gcd;
+        this.d = d / gcd;
+    }
+
+    // Získej nový zlomek, který bude součtem
+    public Fraction add(Fraction other)
+    {
+        return new Fraction(
+                n * other.d + other.n * d,
+                d * other.d);
     }
 
     public static Fraction parse(String s)
     {
-        return new Fraction(1,1);
+        s = s.replace(" ","");
+        String[] split = s.split(Pattern.quote("+"));
+        Fraction sum = new Fraction(0,1);
+        for (String part : split) {
+            Fraction partFraction;
+            if (part.contains("%")) {
+                partFraction = new Fraction(
+                     Long.parseLong(part.replace("%","")),
+                     100);
+            } else {
+                String[] split2 = part.split("/");
+                partFraction = new Fraction(
+                    Long.parseLong(split2[0]),
+                    Long.parseLong(split2[1])
+                );
+            }
+            sum = sum.add(partFraction);
+        }
+        return sum;
     }
 
     public String toString() {
